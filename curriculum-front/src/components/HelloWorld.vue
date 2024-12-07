241df2c53f6ef20871cac418f64e9654d3014992
antarikshgosain <antariksh.gosain@gmail.com>
2024-12-07 18:55:09 -0500

    vue file committed finally

---
+++ curriculum-front/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+/* Add any styles here if needed */
+</style>
